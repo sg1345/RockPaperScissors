647db037b87cc76b1bb6f778de40e82ed995c033
sg1345 <dimitkostz@gmail.com>
2024-07-01 23:49:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RockPaperScissors
+ This is a simple console game of "Rock Paper Scissors".
---
+++ RockPaperScissorsGame/RockPaperScissorsGame/RockPaperScissorsGame.cs
@@ -0,0 +1,10 @@
+﻿namespace RockPaperScissorsGame
+{
+    internal class RockPaperScissorsGame
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
